dfcef0b574de4bfcc6d0587dc4f1b420ae2aa87a
an1tahung <anitalhung@gmail.com>
2015-12-02 23:36:23 +0000

    Added javascript to add new contact to list

--- app/actions.rb
+++ app/actions.rb
@@ -7,4 +7,18 @@ get '/contacts' do
   Contact.all.to_json
 end
 
+post '/new_contact' do 
+  results = {result: false}
+  firstname = params[:firstname]
+  lastname = params[:lastname]
+  email = params[:email]
 
+  contact = Contact.new firstname: firstname, lastname: lastname, email: email
+
+  if contact.save
+    results[:result] = true
+  end
+
+  results.to_json
+
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -8,13 +8,29 @@ $(function() {
         $('<td>').text(contact.email).appendTo(tr);
     },
     receiveContacts: function(contacts) {
+      handlers.container.empty();
       $.each(contacts, handlers.addContact);
     },
     getContacts: function(){
-      handlers.container.empty();
       $.getJSON('/contacts', handlers.receiveContacts);
     }
   };
+
   $('#load_contacts').on('click', handlers.getContacts);
+
+  $('#create_contact').on('click', function(){
+    var fname = $('#firstname').val();
+    var lname = $('#lastname').val();
+    var email = $('#email').val();
+    var contact = {firstname: fname, lastname: lname, email: email};
+
+    $.post('/new_contact', contact, function(data){
+        if(data.result) {
+          handlers.addContact(0, contact);
+        } else {
+          alert("This contact cannot be added! Please check your inputs.");
+        }
+    }, 'json');
+  });
 });
 
